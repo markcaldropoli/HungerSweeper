0f88940808c309fa05173279b26adc6e5a253ee2
Marc Shelton <mshelto3@binghamton.edu>
2018-02-10 21:48:28 -0500

    DED

--- src/PlayerManager.java
+++ src/PlayerManager.java
@@ -186,6 +186,11 @@ public class PlayerManager {
 				int damage = (int) (Math.random() * 20 + 1);
 				System.out.println(b.getName() + " takes " + damage + " pts damage!");
 				b.setHealth(b.getHealth() - damage);
+				if(b.getHealth() < 0){
+					System.out.println(a.getName() + "  killed " + b.getName());
+					removePlayer(b.getName());
+					break;
+				}
 				System.out.println(b.getHealth());
 			}
 			if(one == false){
@@ -204,6 +209,11 @@ public class PlayerManager {
 				int damage = (int) (Math.random() * 20 + 1);
 				System.out.println(a.getName() + " takes " + damage + " pts of damage!");
 				a.setHealth(a.getHealth() - damage);
+				if(a.getHealth() < 0){
+					System.out.println(b.getName() + "  killed " + a.getName());
+					removePlayer(a.getName());
+					break;
+				}
 				System.out.println(a.getHealth());
 			}
 			if(two == false){
