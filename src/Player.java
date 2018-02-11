ac4d378eb51e01ffcfc808579226873541e7ebaf
Marc Shelton <mshelto3@binghamton.edu>
2018-02-10 22:09:17 -0500

    FIRST

--- src/Player.java
+++ src/Player.java
@@ -51,7 +51,6 @@ public class Player {
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		System.out.println(name + ", please input a direction: ");
-		System.out.println("You are at " + x + " , " + y);
 		System.out.println("(up,down,right,left)");
 		
 		while(true) {
