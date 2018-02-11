c01cbfd3236633fcdfda10d0aa91efa7d23d20a6
Mark Caldropoli <mcaldro1@binghamton.edu>
2018-02-10 21:14:40 -0500

    Create Game

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+public class Game {
+	public static void main(String[] args) {
+		PlayerManager pm = new PlayerManager();
+		pm.addPlayer();
+		pm.setTraps();
+		while(pm.getPlayers().size() != 1) {
+			pm.move();
+		}
+	}
+}
